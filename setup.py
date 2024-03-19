4a2c68a2a49efa89e890f7ae56d3288789e22cda
Alexandre-Delplanque <alexandre.delplanque.ca@gmail.com>
2024-03-19 09:59:26 +0100

    fix: update code version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='animaloc',
-    version='0.2.0',
+    version='0.2.1',
     license='MIT License',
     license_files = ('LICENSE.md'),
     description='Animal localization in aerial imagery using Pytorch',
